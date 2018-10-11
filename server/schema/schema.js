f8dc8674ced127d6d6a4d294889c796e747150e0
Frank <xuzy213@gmail.com>
2018-10-12 12:47:33 +1300

    add resolve of BookType AuthorType and RootQuery

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -21,7 +21,7 @@ const BookType = new GraphQLObjectType({
     author: {
       type: AuthorType,
       resolve (parent, args) {
-        // return _.find(authors, {id: parent.authorid})
+        return Author.findById(parent.authorid)
       }
     }
   })
@@ -36,7 +36,7 @@ const AuthorType = new GraphQLObjectType({
     books: {
       type: new GraphQLList(BookType),
       resolve (parent, args) {
-        // return _.filter(books, {authorid: parent.id})
+        return Book.find({authorid: parent.id})
       }
     }
   })
@@ -48,28 +48,27 @@ const RootQuery = new GraphQLObjectType({
     books: {
       type: new GraphQLList(BookType),
       resolve (parent, args) {
-        // return books
+        return Book.find({})
       }
     },
     authors: {
       type: new GraphQLList(AuthorType),
       resolve (parent, args) {
-        // return authors
+        return Author.find({})
       }
     },
     book: {
       type: BookType,
       args: {id: {type: GraphQLID}},
       resolve (parent, args) {
-        // code to get data from db / other source
-        // return _.find(books, {id: args.id})
+        return Book.findById(args.id)
       }
     },
     author: {
       type: AuthorType,
       args: {id: {type: GraphQLID}},
       resolve (parent, args) {
-        // return _.find(authors, {id: args.id})
+        return Author.findById(args.id)
       }
     }
   }
