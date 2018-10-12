9768b9355ccf34b5b716f3bf4103976db03d0f8a
Frank <xuzy213@gmail.com>
2018-10-12 13:33:14 +1300

    init AuthorList

---
+++ client/src/components/AuthorList.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class AuthorList extends Component {
+  render() {
+    return (
+      <div className="AuthorList">
+        <ul id="author-list">
+          <li>Author name</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default AuthorList;
